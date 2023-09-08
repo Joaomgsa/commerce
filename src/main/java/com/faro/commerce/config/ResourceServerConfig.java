3973077e61acdd7cbf769299b433f7f5ab40fc2d
joaomgsa <joaomgsa@gmail.com>
2023-09-08 20:10:09 -0300

    access control

--- src/main/java/com/faro/commerce/config/ResourceServerConfig.java
+++ src/main/java/com/faro/commerce/config/ResourceServerConfig.java
@@ -1,22 +1,23 @@
 package com.faro.commerce.config;
 
 
-import java.util.Arrays;
-
 import org.springframework.beans.factory.annotation.Autowired;
-
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
-import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+import java.util.Arrays;
 
 
 @Configuration
 @EnableResourceServer
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+
 
 
     @Autowired
@@ -25,7 +26,6 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
     @Autowired
     private JwtTokenStore tokenStore;
 
-    public static final String[] PUBLIC = {"/oauth/token", "/h2-console/**"};
 
 
     @Override
@@ -36,9 +36,7 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
             http.headers().frameOptions().disable();
         }
 
-        http.authorizeRequests()
-                .antMatchers(PUBLIC).permitAll()
-                .anyRequest().authenticated();
+        http.authorizeRequests().anyRequest().permitAll();
 
     }
 
